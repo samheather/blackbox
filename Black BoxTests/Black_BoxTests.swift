8743214d8ea950517320494365e66e1aa910f6b9
Sam Heather <sam@heather.sh>
2016-02-06 16:23:26 +0000

    Initial Commit

---
+++ Black BoxTests/Black_BoxTests.swift
@@ -0,0 +1,36 @@
+//
+//  Black_BoxTests.swift
+//  Black BoxTests
+//
+//  Created by Samuel B Heather on 06/02/2016.
+//  Copyright © 2016 Samuel Brendon Heather. All rights reserved.
+//
+
+import XCTest
+@testable import Black_Box
+
+class Black_BoxTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
