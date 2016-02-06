6ef612a9d069ce89d8a94d668332e08debee529b
Sam Heather <sam@heather.sh>
2016-02-06 20:29:34 +0000

    Added a draft InSession view controller

---
+++ Black Box/InSessionViewController.swift
@@ -0,0 +1,101 @@
+//
+//  InSessionViewController.swift
+//  Black Box
+//
+//  Created by Samuel B Heather on 06/02/2016.
+//  Copyright © 2016 Samuel Brendon Heather. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class InSessionViewController: UIViewController {
+    
+    var sessionKey:String!
+    var pingTimer:NSTimer!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "\"In Session\""
+        
+        // Set session key
+        sessionKey = "abc"
+        
+        // Start the Location Service
+        LocationService.sharedInstance.startUpdatingLocation()
+        
+        pingTimer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: Selector("ping"), userInfo: nil, repeats: true)
+        
+    }
+    
+    func ping() {
+        let position = LocationService.sharedInstance.getLastLocation()
+
+        let pingTime:Int = Int(NSTimeIntervalSince1970)
+        let lat:Double = position.coordinate.latitude
+        let long:Double = position.coordinate.longitude
+        let speed:Double = position.speed
+        let bearing:Double = position.course
+        let altitude:Double = position.altitude
+        let accuracy_location:Double = position.horizontalAccuracy
+        let accuracy_altitude:Double = position.verticalAccuracy
+        
+        if !savePing(pingTime, lat: lat, long: long, speed: speed, bearing: bearing, altitude: altitude, accuracy_location: accuracy_location, accuracy_altitude: accuracy_altitude) {
+            print("Ping didn't save successfully")
+        }
+        else {
+            print("Ping saved successfully")
+        }
+        
+    }
+    
+    func savePing(pingTime:Int, lat:Double, long:Double, speed:Double,
+        bearing:Double, altitude:Double, accuracy_location:Double,
+        accuracy_altitude:Double) -> Bool {
+            //1
+            let appDelegate =
+            UIApplication.sharedApplication().delegate as! AppDelegate
+            
+            let managedContext = appDelegate.managedObjectContext
+            
+            //2
+            let entity =  NSEntityDescription.entityForName("Ping",
+                inManagedObjectContext:managedContext)
+            
+            let ping = NSManagedObject(entity: entity!,
+                insertIntoManagedObjectContext: managedContext)
+            
+            //3
+            ping.setValue(pingTime, forKey: "pingTime")
+            ping.setValue(lat, forKey: "lat")
+            ping.setValue(long, forKey: "long")
+            ping.setValue(speed, forKey: "speed")
+            ping.setValue(bearing, forKey: "bearing")
+            ping.setValue(altitude, forKey: "altitude")
+            ping.setValue(accuracy_location, forKey: "accuracy_location")
+            ping.setValue(accuracy_altitude, forKey: "accuracy_altitude")
+            
+            //4
+            do {
+                try managedContext.save()
+                //5
+                return true
+            } catch let error as NSError  {
+                print("Could not save \(error), \(error.userInfo)")
+                return false
+            }
+            return false
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        LocationService.sharedInstance.stopUpdatingLocation()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
---
+++ Black Box/LocationService.swift
@@ -0,0 +1,57 @@
+//
+//  LocationService.swift
+//  Black Box
+//
+//  Created by Samuel B Heather on 06/02/2016.
+//  Copyright © 2016 Samuel Brendon Heather. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+class LocationService: NSObject, CLLocationManagerDelegate {
+    static let sharedInstance = LocationService()
+    
+    var lastLocation:CLLocation! = nil
+    
+    let locationManager = CLLocationManager()
+    
+    override init() {
+        super.init()
+        
+        // Ask for Authorisation from the User.
+        self.locationManager.requestAlwaysAuthorization()
+        
+        //        // For use in foreground
+        //        self.locationManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            print("location srvs enabled")
+            locationManager.delegate = self
+            locationManager.distanceFilter = kCLDistanceFilterNone
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        }
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        lastLocation = manager.location
+    }
+    
+    func getLastLocationFormattedGeoJson() -> Dictionary<String, AnyObject> {
+        let lat = lastLocation.coordinate.latitude
+        let lon = lastLocation.coordinate.longitude
+        return ["type":"Point", "coordinates": [lat, lon]];
+    }
+    
+    func getLastLocation() -> CLLocation! {
+        return lastLocation
+    }
+    
+    func startUpdatingLocation() {
+        locationManager.startUpdatingLocation()
+    }
+    
+    func stopUpdatingLocation() {
+        locationManager.stopUpdatingLocation()
+    }
+}
